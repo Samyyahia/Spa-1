cb3d9b49480d6374b0826b144136598ca7f048ba
Samy Yahia <syahia93250@yahoo.com>
2020-03-06 16:54:27 +0100

    Up

--- app/js/app.js
+++ app/js/app.js
@@ -1,53 +1,65 @@
 import $ from 'jquery';
+import slick from 'slick-carousel';
 
-(function($) {
-  $(document).ready(function($) {
-    let app = {
-        init: function () {
-          if(window.addEventListener){
-              window.addEventListener("scroll", function() {
-                  app.animate();
-              }, false);
-          }else{
-              window.attachEvent("scroll", function(){
-                  app.animate();
-              });
-          }
-
-          app.checkIE();
-          app.animate();
-        },
-
-        checkIE : function(){
-          var nav =   window.navigator.userAgent,
-              idx =   nav.indexOf("MSIE"),
-              tri =   nav.indexOf("Trident/");
-          if(idx > 0 || tri > 0){
-              var version = (parseInt(nav.substring(idx + 5, nav.indexOf('.', idx)), 10));
-              app.ie  =   true;
-              document.body.classList.add('ie');
-              document.body.classList.add('ie'+version);
-          }
-        },
-        animate: function(){
-          $(".wa").each(function (i, v) {
-            var scrollTop    = $(window).scrollTop(),
-                el           = $(this),
-                elHeight     = el.height(),
-                winHeight    = $(window).height(),
-                offsetTop    = el.offset().top + parseInt(el.css("padding-top")),
-                bottomScreen = scrollTop + winHeight;
-
-            if ( bottomScreen >= offsetTop ) {
-              el.addClass("animate");
-            } else {
-              el.removeClass("animate");
-            }
-          });
-      },
-    }
-
-    app.init();
-  });
-
-})($);
+(function($, slick) {
+    $(document).ready(function($) {
+        let app = {
+            init: function () {
+                if(window.addEventListener){
+                    window.addEventListener("scroll", function() {
+                        app.animate();
+                    }, false);
+                }else{
+                    window.attachEvent("scroll", function(){
+                        app.animate();
+                    });
+                }
+                
+                if ($('#slider'))
+                    app.slider($('#slider'))
+                
+                app.checkIE();
+                app.animate();
+            },
+            
+            slider: function (item) {
+                item.slick({
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: false,
+                })
+            },
+            
+            checkIE : function(){
+                var nav =   window.navigator.userAgent,
+                    idx =   nav.indexOf("MSIE"),
+                    tri =   nav.indexOf("Trident/");
+                if(idx > 0 || tri > 0){
+                    var version = (parseInt(nav.substring(idx + 5, nav.indexOf('.', idx)), 10));
+                    app.ie  =   true;
+                    document.body.classList.add('ie');
+                    document.body.classList.add('ie'+version);
+                }
+            },
+            animate: function(){
+                $(".wa").each(function (i, v) {
+                    var scrollTop    = $(window).scrollTop(),
+                        el           = $(this),
+                        elHeight     = el.height(),
+                        winHeight    = $(window).height(),
+                        offsetTop    = el.offset().top + parseInt(el.css("padding-top")),
+                        bottomScreen = scrollTop + winHeight;
+                    
+                    if ( bottomScreen >= offsetTop ) {
+                        el.addClass("animate");
+                    } else {
+                        el.removeClass("animate");
+                    }
+                });
+            },
+        }
+        
+        app.init();
+    });
+    
+})($, slick);
--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,66 @@
-const babelify    = require('babelify');
-const browserify  = require('browserify');
-const buffer      = require('vinyl-buffer');
-const concat      = require('gulp-concat');
-const del         = require('del');
-const gulp        = require('gulp');
-const imagemin    = require('gulp-imagemin');
-const gulpif      = require('gulp-if');
-const minifyCSS   = require('gulp-csso');
-const sass        = require('gulp-sass');
-const source      = require('vinyl-source-stream');
-const sourcemaps  = require('gulp-sourcemaps');
-const sync        = require('browser-sync').create();
-const uglify      = require('gulp-uglify');
-const autoprefixer= require('gulp-autoprefixer');
-const pxtorem     = require('gulp-pxtorem');
-const cleaner     = require('gulp-clean');
-const plumber     = require('gulp-plumber');
+const babelify      = require('babelify');
+const browserify    = require('browserify');
+const buffer        = require('vinyl-buffer');
+const concat        = require('gulp-concat');
+const del           = require('del');
+const gulp          = require('gulp');
+const imagemin      = require('gulp-imagemin');
+const gulpif        = require('gulp-if');
+const minifyCSS     = require('gulp-csso');
+const sass          = require('gulp-sass');
+const source        = require('vinyl-source-stream');
+const sourcemaps    = require('gulp-sourcemaps');
+const sync          = require('browser-sync').create();
+const uglify        = require('gulp-uglify');
+const autoprefixer  = require('gulp-autoprefixer');
+const pxtorem       = require('gulp-pxtorem');
+const cleaner       = require('gulp-clean');
+const plumber       = require('gulp-plumber');
 const notify     	= require('gulp-notify');
-const gutil     	= require('gulp-util');
+const gutil         = require('gulp-util');
 
 const isProd = process.env.NODE_ENV === 'production';
 
 const paths = {
     css: {
-      src: [
-        './app/scss/style.scss',
-      ],
-      out: 'style.min.css',
-      dist: './dist/css'
+        src: [
+            './app/scss/style.scss',
+            './node_modules/slick-carousel/slick/slick.scss',
+            './node_modules/slick-carousel/slick/slick-theme.scss'
+        ],
+        out: 'style.min.css',
+        dist: './dist/css'
     },
     js: {
-      src: [
-        './app/js/app.js'
-      ],
-      out: 'app.min.js',
-      dist: 'dist/js'
+        src: [
+            './app/js/app.js'
+        ],
+        out: 'app.min.js',
+        dist: 'dist/js'
     },
     img: {
-      src: './app/img/**/*',
-      dist: './dist/img'
+        src: './app/img/**/*',
+        dist: './dist/img'
     },
     fonts: {
-      src: './app/fonts/**/*',
-      dist: './dist/fonts'
+        src: './app/fonts/**/*',
+        dist: './dist/fonts'
     },
     html: {
-      src: './app/**/*.html',
-      dist: './dist/'
+        src: './app/**/*.html',
+        dist: './dist/'
     },
     dist: [
-      'dist'
+        'dist'
     ],
     server: {
-      baseDir: './dist'
+        baseDir: './dist'
     },
     watch: {
-      css: 'app/**/*.scss',
-      js: 'app/js/*.js',
-      html: 'app/**/*.html',
-      img: 'app/img/*'
+        css: 'app/**/*.scss',
+        js: 'app/js/*.js',
+        html: 'app/**/*.html',
+        img: 'app/img/*'
     }
 };
 
@@ -67,23 +69,23 @@ const paths = {
  */
 
 function scss() {
-  return gulp.src(paths.css.src)
-		.pipe(plumber({ errorHandler: function(err) {
-			notify.onError({
-				title: "Gulp error in " + err.plugin,
-				message:  err.toString()
-			})(err);
-
-			gutil.beep();
-		}}))
-    .pipe(concat(paths.css.out))
-    .pipe(gulpif(!isProd, sourcemaps.init()))
-    .pipe(sass())
-    .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false }))
-    .pipe(pxtorem())
-    .pipe(minifyCSS())
-    .pipe(gulp.dest(paths.css.dist))
-    .pipe(sync.stream());
+    return gulp.src(paths.css.src)
+        .pipe(plumber({ errorHandler: function(err) {
+                notify.onError({
+                    title: "Gulp error in " + err.plugin,
+                    message:  err.toString()
+                })(err);
+                
+                gutil.beep();
+            }}))
+        .pipe(concat(paths.css.out))
+        .pipe(gulpif(!isProd, sourcemaps.init()))
+        .pipe(sass())
+        .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false }))
+        .pipe(pxtorem())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(paths.css.dist))
+        .pipe(sync.stream());
 }
 
 /**
@@ -91,23 +93,23 @@ function scss() {
  */
 
 function js(done) {
-	return browserify({entries: paths.js.src, debug: true})
-		.transform(babelify, {presets: 'es2015'})
-		.bundle()
-		.on('error', function(err) {
-			notify.onError({
-				title: "Gulp error in " + err.filename,
-				message:  err.message
-			})(err);
-
-			gutil.beep();
-			done();
-		})
-		.pipe(source(paths.js.out))
-		.pipe(buffer())
-		.pipe(uglify())
-		.pipe(gulp.dest(paths.js.dist))
-		.pipe(sync.stream());
+    return browserify({entries: paths.js.src, debug: true})
+        .transform(babelify, {presets: 'es2015'})
+        .bundle()
+        .on('error', function(err) {
+            notify.onError({
+                title: "Gulp error in " + err.filename,
+                message:  err.message
+            })(err);
+            
+            gutil.beep();
+            done();
+        })
+        .pipe(source(paths.js.out))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.js.dist))
+        .pipe(sync.stream());
 };
 
 /**
@@ -115,9 +117,9 @@ function js(done) {
  */
 
 function images() {
-  return gulp.src(paths.img.src)
-    .pipe(gulpif(isProd, imagemin({verbose: true})))
-    .pipe(gulp.dest(paths.img.dist));
+    return gulp.src(paths.img.src)
+        .pipe(gulpif(isProd, imagemin({verbose: true})))
+        .pipe(gulp.dest(paths.img.dist));
 }
 
 /**
@@ -125,16 +127,16 @@ function images() {
  */
 
 function fonts() {
-  return gulp.src(paths.fonts.src)
-    .pipe(gulp.dest(paths.fonts.dist));
+    return gulp.src(paths.fonts.src)
+        .pipe(gulp.dest(paths.fonts.dist));
 }
 
 /**
-* HTML
-*/
+ * HTML
+ */
 function html() {
-  return gulp.src(paths.html.src)
-  .pipe(gulp.dest(paths.html.dist));
+    return gulp.src(paths.html.src)
+        .pipe(gulp.dest(paths.html.dist));
 }
 
 /**
@@ -142,7 +144,7 @@ function html() {
  */
 
 function clean() {
-  return del(paths.dist);
+    return del(paths.dist);
 }
 
 
@@ -150,14 +152,14 @@ function clean() {
 gulp.task('build', gulp.series(clean, gulp.parallel(html, scss, js, images, fonts)));
 
 gulp.task('default', gulp.parallel(html, scss, js, images, fonts, function(done) {
-  sync.init({
-    server: paths.server
-  });
-
-  gulp.watch(paths.watch.css, scss);
-  gulp.watch(paths.watch.js, js);
-  gulp.watch(paths.watch.html, html);
-  gulp.watch(paths.watch.img, images);
-
-  done();
+    sync.init({
+        server: paths.server
+    });
+    
+    gulp.watch(paths.watch.css, scss);
+    gulp.watch(paths.watch.js, js);
+    gulp.watch(paths.watch.html, html);
+    gulp.watch(paths.watch.img, images);
+    
+    done();
 }));
